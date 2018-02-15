e0baadef3373474304f9b4b5c0bc6be69964946c
Guilherme Moreira <guilhermegms.silva@gmail.com>
2018-02-14 22:16:21 -0200

    feat(App)
    * Adiciona rotas

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import { Link, Route } from 'react-router-dom'
 import * as BooksAPI from './utils/BooksAPI.js';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar'
+import AddBookPage from './modules/AddBookPage'
 import Library from './modules/Library/Library'
-import Book from './modules/Book/Book';
-import Shelf from './modules/Shelf/Shelf';
 class App extends Component {
 
   render() {
     return (
       <MuiThemeProvider>
-        <Library BooksAPI={BooksAPI} />
+        <Route exact path='/' render={() => (
+          <Library BooksAPI={BooksAPI} />
+        )} />
+        <Route exact path='/new' render={() => (
+          <AddBookPage BooksAPI={BooksAPI}/>
+        )} />
       </MuiThemeProvider>
     )
   }
