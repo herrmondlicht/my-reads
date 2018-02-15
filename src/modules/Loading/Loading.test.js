871feebfa27f2904e2b293fa12e6b479e50ab9a7
Guilherme Moreira <guilhermegms.silva@gmail.com>
2018-02-14 22:19:10 -0200

    refactor(Loading)
    * Altera it para test

--- src/modules/Loading/Loading.test.js
+++ src/modules/Loading/Loading.test.js
@@ -4,10 +4,10 @@ import Loading from './Loading';
 
 describe('Loading', () => {
   const renderedLoading = shallow(Loading).withProps();
-  it('must return a function', () => {
+  test('must return a function', () => {
     expect(typeof Loading).toBe('function');
   });
-  it('must render a CircularProgress', () => {
+  test('must render a CircularProgress', () => {
     const actual = renderedLoading.find('CircularProgress').length
       , expected = 1;
     expect(actual).toBe(expected);
