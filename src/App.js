09dcbf55f83ba23aecae83b8129822fb9fe64538
Guilherme Moreira <guilhermegms.silva@gmail.com>
2018-02-15 22:05:47 -0200

    refactor(App)
    * Adicionado div wrapper

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar'
 import AddBookPage from './modules/AddBookPage'
 import Library from './modules/Library/Library'
+
 class App extends Component {
 
   render() {
     return (
       <MuiThemeProvider>
-        <Route exact path='/' render={() => (
-          <Library BooksAPI={BooksAPI} />
-        )} />
-        <Route exact path='/new' render={() => (
-          <AddBookPage BooksAPI={BooksAPI}/>
-        )} />
+        <div>
+          <Route exact path='/' render={() => (
+            <Library BooksAPI={BooksAPI} />
+          )} />
+          <Route exact path='/new' render={() => (
+            <AddBookPage BooksAPI={BooksAPI} />
+          )} />
+        </div>
       </MuiThemeProvider>
     )
   }
