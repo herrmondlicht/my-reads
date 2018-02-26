bcfa39932883c01474d7012ddf4258e0d3e9b05c
Guilherme Moreira <guilhermegms.silva@gmail.com>
2018-02-26 20:37:10 -0300

    test(AddBook)
    * Adiciona testes no AddBook

--- src/modules/AddBookPage/AddBookPage.test.js
+++ src/modules/AddBookPage/AddBookPage.test.js
@@ -6,10 +6,13 @@ import * as OriginalBooksAPI from '../../utils/BooksAPI';
 
 describe('AddBookPage', () => {
   const BooksAPI = stub(OriginalBooksAPI)
-  , loadedBooks = [
-    
-  ]
-    , renderTest = (props) => shallow(AddBookPage).withProps({ ...props, BooksAPI })
+    , loadedBooks = [
+      { id: 1, title: 'TITLE 1', subtitle: 'SUBTITLE 1', shelf: 'old' },
+      { id: 2, title: 'TITLE 2', subtitle: 'SUBTITLE 2', shelf: 'old' },
+      { id: 3, title: 'TITLE 3', subtitle: 'SUBTITLE 3', shelf: 'old' }
+    ]
+    , renderTest = (props) => shallow(AddBookPage).withProps({ ...props, BooksAPI, books: loadedBooks })
+
   test('must be a function', () => {
     expect(typeof AddBookPage).toBe('function')
   });
@@ -70,17 +73,89 @@ describe('AddBookPage', () => {
       let stateAfterSearch;
 
       await HandleSearchFunction(wrongText)
-      
+
       stateAfterSearch = wrapper.state()
       expect(stateAfterSearch).toEqual(expectedState)
     })
   })
 
-  describe('must have a FlagBooksShelves', () => {
+  //TODO: AddBook and updateBookList 
+
+  describe('must have a GetBooksWithShelfFlag', () => {
 
     test('that updates the result from search with the data from loadedBooks', () => {
+      const wrapper = renderTest()
+        , booksGotFromSearch = [
+          { id: 1, title: 'TITLE 1', subtitle: 'SUBTITLE 1' },
+          { id: 2, title: 'TITLE 2', subtitle: 'SUBTITLE 2' },
+          { id: 3, title: 'TITLE 3', subtitle: 'SUBTITLE 3' }
+        ]
+        , GetBooksWithShelfFlagFunction = wrapper.instance().GetBooksWithShelfFlag
+
+      wrapper.setState({ bookList: booksGotFromSearch })
+
+      const arrayWithFlaggedBooks = GetBooksWithShelfFlagFunction()
+
+      expect(arrayWithFlaggedBooks).toEqual(loadedBooks)
 
     })
 
+  })
+
+  describe('must have a AddBook method', () => {
+    BooksAPI.update = stub().resolves({})
+    beforeEach(() => {
+      BooksAPI.update.resetHistory()
+    })
+    test('that adds a book to the user\'s shelf and updates the bookList', async () => {
+      const wrapper = renderTest()
+        , instance = wrapper.instance()
+        , stubUpdateBookList = stub(instance, 'UpdateBookList')
+        , addBookFunction = instance.AddBook
+        , bookToBeAdded = {
+          id: 1,
+          title: 'TITLE 1',
+          subtitle: 'SUBTITLE 1',
+          shelf: 'oldShelf'
+        }
+        , newShelf = 'newShelf'
+
+      await addBookFunction(bookToBeAdded, newShelf)
+
+      assert.calledWith(BooksAPI.update, bookToBeAdded,newShelf)
+      assert.calledWith(stubUpdateBookList,bookToBeAdded, newShelf)
+
+    })
+  })
+
+  describe('must have a UpdateBookList method', () => {
+
+    test('that updates the selected search result book shelf', () => {
+      const wrapper = renderTest()
+        , instance = wrapper.instance()
+        , bookAlreadyOnShelf = {
+          id: 1,
+          title: 'TITLE 1',
+          subtitle: 'SUBTITLE 1',
+          shelf: 'oldShelf'
+        }
+        , updateBookListFunction = instance.UpdateBookList
+        , newShelf = 'newShelf'
+
+      wrapper.setState({
+        bookList: [bookAlreadyOnShelf]
+      })
+      const stubSetState = stub(instance, 'setState')
+
+      updateBookListFunction(bookAlreadyOnShelf, newShelf)
+
+      const expected = {
+        bookList: [{
+          ...bookAlreadyOnShelf,
+          shelf: newShelf,
+        }]
+      }
+      assert.calledWith(stubSetState, expected)
+    })
   })
 })
